8f518f08b9213b69d1186b4a7d2e45d7fe93e7a6
kaito <kaitokun206203@gmail.com>
2025-01-02 18:52:48 +0700

    Update

---
+++ lib/Widget/food_items_display.dart
@@ -0,0 +1,122 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:recipe_app/Provider/favorite_provider.dart';
+import 'package:recipe_app/Views/recipe_detail_screen.dart';
+import 'package:iconsax/iconsax.dart';
+
+class FoodItemsDisplay extends StatelessWidget {
+  final DocumentSnapshot<Object?> documentSnapshot;
+  const FoodItemsDisplay({super.key, required this.documentSnapshot});
+
+  @override
+  Widget build(BuildContext context) {
+    final provider = FavoriteProvider.of(context);
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) =>
+                RecipeDetailScreen(documentSnapshot: documentSnapshot),
+          ),
+        );
+      },
+      child: Container(
+        margin: const EdgeInsets.only(right: 10),
+        width: 230,
+        child: Stack(
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  width: double.infinity,
+                  height: 160,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15),
+                    image: DecorationImage(
+                      fit: BoxFit.cover,
+                      image: NetworkImage(
+                        //documentSnapshot['image'],
+                        'https://mediterraneandelicacies.co.za/cdn/shop/products/black-sliced-olives-3kg-tin-not-suitable-for-table-olives-18012927983771.jpg?v=1616196293'
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 10),
+                Text(
+                  documentSnapshot['name'],
+                  style: const TextStyle(
+                    fontSize: 17,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(height: 5),
+                Row(
+                  children: [
+                    const Icon(
+                      Iconsax.flash_1,
+                      size: 16,
+                      color: Colors.grey,
+                    ),
+                    Text(
+                      "${documentSnapshot['cal']} Cal",
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 12,
+                        color: Colors.grey,
+                      ),
+                    ),
+                    const Text(
+                      " · ",
+                      style: TextStyle(
+                        fontWeight: FontWeight.w900,
+                        color: Colors.grey,
+                      ),
+                    ),
+                    const Icon(
+                      Iconsax.clock,
+                      size: 16,
+                      color: Colors.grey,
+                    ),
+                    const SizedBox(width: 5),
+                    Text(
+                      "${documentSnapshot['time']} Min",
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 12,
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            Positioned(
+              top: 5,
+              right: 5,
+              child: CircleAvatar(
+                radius: 18,
+                backgroundColor: Colors.white,
+                child: InkWell(
+                  onTap: () {
+                    provider.toggleFavorite(documentSnapshot);
+                  },
+                  child: Icon(
+                    provider.isExist(documentSnapshot)
+                        ? Iconsax.heart5
+                        : Iconsax.heart,
+                    color: provider.isExist(documentSnapshot)
+                        ? Colors.red
+                        : Colors.black,
+                    size: 20,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
